3e3f869b5257833ef42ada26fa2f6143d538f9c0
Vanessa Schneider <vanessajwade@gmail.com>
2022-02-22 10:01:40 -0500

    commit tree eeb71c6bacdc2464c73d344164c9e758c0600339

---
+++ app.rb
@@ -0,0 +1,26 @@
+def greet_programmer
+    puts "Hello programmer!"
+end
+
+
+def greet (name)
+    puts "Hello ${name}"
+end
+
+
+def greet_with_default (name="programmer")
+    puts "Hello${name}"
+end
+
+def add(num1, num2)
+    puts num1 + num2
+end
+
+def half(num1)
+    if num.class !=Integer
+        return nil
+    end
+
+    num / 2
+
+end
